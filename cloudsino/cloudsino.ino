a15e6278331672b7f85db3a67f61400342e79ca7
fthouin <felix.thouin@gmail.com>
2020-05-10 16:05:00 -0400

    First commit! One pattern of LED

---
+++ cloudsino/cloudsino.ino
@@ -0,0 +1,175 @@
+#include <FastLED.h>
+#define NUM_LEDS 300
+#define DATA_PIN 6
+#define MAX_DIVIDER 16
+#define INIT 10
+#define SPEED 1
+#define COLORSWITCH 4
+
+const int pinSelect=10;
+const int pinUp=11;
+const int pinDown=12;
+
+int selectState;
+int upState;
+int downState;
+
+int currentUp;
+int currentDown;
+
+unsigned int central[]={0,0,0,0};
+unsigned int cloudIndices[]={0,69,140,218};
+unsigned int skyIndices[]={42,108,180,262};
+unsigned int stripLength=0;
+unsigned int widthPattern=8;
+unsigned int divider=1;
+unsigned int turncnt=0;
+unsigned int readingPot;
+unsigned int lower;
+unsigned int upper;
+unsigned int switchDelay=1;
+unsigned int cloudColorIndex=0;
+unsigned int skyColorIndex=2;
+CRGB leds[NUM_LEDS];
+
+CRGB predefColors[]={CRGB::MediumVioletRed,CRGB::MidnightBlue,CRGB::SpringGreen,CRGB::Salmon,CRGB::Tomato,CRGB::Yellow,CRGB::Crimson,CRGB::GreenYellow,CRGB::MediumBlue,CRGB::Teal};
+int colorNumber=sizeof(predefColors)/sizeof(CRGB);
+void setup() 
+{ 
+  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS); 
+  pinMode(pinSelect,INPUT);
+  pinMode(pinUp,INPUT);
+  pinMode(pinDown,INPUT);
+  selectState=digitalRead(pinSelect);
+  upState=digitalRead(pinUp);
+  downState=digitalRead(pinDown);
+}
+void loop() {
+for (int k=0; k<4; k++) 
+  {
+    stripLength=skyIndices[k]-cloudIndices[k];
+    for (int i=0; i<widthPattern; i++) 
+    {
+      leds[(central[k]+i)%stripLength+cloudIndices[k]]=predefColors[cloudColorIndex];
+      leds[(central[k]+i)%stripLength+cloudIndices[k]].maximizeBrightness();
+      leds[(central[k]+i)%stripLength+cloudIndices[k]]/=2;
+    }
+    lower=skyIndices[k];
+    if(k==3)
+    {
+      upper=NUM_LEDS;  
+    }
+    else{
+      upper=cloudIndices[k+1];
+    }
+      
+    for(int j=lower; j<upper;j++)
+    {
+      leds[j]=predefColors[skyColorIndex];
+      leds[j]/=4;
+    }
+    
+  }
+    FastLED.show();
+  for (int k=0; k<4; k++) 
+  {
+    stripLength=skyIndices[k]-cloudIndices[k];
+    for (int i=0; i<widthPattern; i++) 
+    {
+      leds[(central[k]+i)%stripLength+cloudIndices[k]]=CRGB::Black;
+      leds[(central[k]-i)%stripLength+cloudIndices[k]]=CRGB::Black;
+    }
+    lower=skyIndices[k];
+    if(k==3)
+    {
+      upper=NUM_LEDS;  
+    }
+    else{
+      upper=cloudIndices[k+1];
+    }
+      
+    for(int j=lower; j<upper;j++)
+    {
+      leds[j]=CRGB::Black;
+    }
+    
+    
+  }
+  
+  //leds[central]= 
+
+  
+  for (int k=0; k<4; k++) 
+  {
+    stripLength=skyIndices[k]-cloudIndices[k];
+    central[k]=(central[k]+SPEED)%stripLength;
+    
+  }
+ if(central[0]==0)
+    {
+      turncnt++;
+      if(turncnt==COLORSWITCH)
+      {
+        cloudColorIndex=(cloudColorIndex+1)%(colorNumber);
+      }
+      if(turncnt==2*COLORSWITCH)
+      {
+        turncnt=0;
+        skyColorIndex=(skyColorIndex+1)%(colorNumber);
+      }
+    }
+  
+  currentUp=digitalRead(pinUp);
+  currentDown=digitalRead(pinDown);
+  selectState=digitalRead(pinSelect);
+  if(currentUp!=upState | currentDown!=downState )
+  {
+    if(currentUp!=upState)
+    {
+      //Toggled UP
+      if(selectState==HIGH)
+      {
+        //Increase Pattern width
+        widthPattern++;
+      }
+      else
+      {
+        //Switching amplitude
+        if(divider<=MAX_DIVIDER)
+        {
+          divider=divider*2;  
+        }
+      }
+     
+    }
+    else
+    {
+            if(selectState==HIGH)
+      {
+        //Reduce pattern width
+        widthPattern--;
+      }
+      else
+      {
+        //Switching amplitude
+        if(divider!=1)
+        {
+          divider=divider/2;  
+        }
+        
+      }
+      
+    }
+     upState=currentUp;
+     downState=currentDown;
+    // Serial.println(divider);
+  
+  }
+
+//  
+//  leds[0] = CRGB::MidnightBlue; FastLED.show(); delay(30);
+//  leds[0] = CRGB::Black; FastLED.show(); delay(30);
+//  leds[5] = CRGB::MidnightBlue; FastLED.show(); delay(30);
+//  leds[5] = CRGB::Black; FastLED.show(); delay(30);
+  delay(20);
+}
